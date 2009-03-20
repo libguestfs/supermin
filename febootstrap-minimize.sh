d89a55b4a3249e74602f9dc3cd26a8cca3370e7f
rjones <rjones>
2009-03-20 11:49:22 +0000

    More minimization (now 15.9 MB)

--- febootstrap-minimize.sh
+++ febootstrap-minimize.sh
@@ -20,7 +20,7 @@
 
 TEMP=`getopt \
         -o '' \
-        --long help,all,none,keep-locales,drop-locales,keep-docs,drop-docs,keep-yum-cache,drop-yum-cache,keep-cracklib,drop-cracklib,keep-i18n,drop-i18n,keep-zoneinfo,drop-zoneinfo \
+        --long help,all,none,keep-locales,drop-locales,keep-docs,drop-docs,keep-cracklib,drop-cracklib,keep-i18n,drop-i18n,keep-zoneinfo,drop-zoneinfo,keep-rpmdb,drop-rpmdb,keep-yum-cache,drop-yum-cache,keep-services,drop-services,keep-sln,drop-sln,keep-ldconfig,drop-ldconfig \
         -n febootstrap-minimize -- "$@"`
 if [ $? != 0 ]; then
     echo "febootstrap-minimize: problem parsing the command line arguments"
@@ -32,20 +32,28 @@ set_all ()
 {
   keep_locales=no
      keep_docs=no
-keep_yum_cache=no
  keep_cracklib=no
      keep_i18n=no
  keep_zoneinfo=no
+    keep_rpmdb=no
+keep_yum_cache=no
+ keep_services=no
+      keep_sln=no
+ keep_ldconfig=no
 }
 
 set_none ()
 {
   keep_locales=yes
      keep_docs=yes
-keep_yum_cache=yes
  keep_cracklib=yes
      keep_i18n=yes
  keep_zoneinfo=yes
+    keep_rpmdb=yes
+keep_yum_cache=yes
+ keep_services=yes
+      keep_sln=yes
+ keep_ldconfig=yes
 }
 
 set_all
@@ -76,12 +84,6 @@ while true; do
 	--drop-docs)
 	    keep_docs=no
 	    shift;;
-	--keep-yum-cache)
-	    keep_yum_cache=yes
-	    shift;;
-	--drop-yum-cache)
-	    keep_yum_cache=no
-	    shift;;
 	--keep-cracklib)
 	    keep_cracklib=yes
 	    shift;;
@@ -99,6 +101,36 @@ while true; do
 	    shift;;
 	--drop-zoneinfo)
 	    keep_zoneinfo=no
+	    shift;;
+	--keep-rpmdb)
+	    keep_rpmdb=yes
+	    shift;;
+	--drop-rpmdb)
+	    keep_rpmdb=no
+	    shift;;
+	--keep-yum-cache)
+	    keep_yum_cache=yes
+	    shift;;
+	--drop-yum-cache)
+	    keep_yum_cache=no
+	    shift;;
+	--keep-services)
+	    keep_services=yes
+	    shift;;
+	--drop-services)
+	    keep_services=no
+	    shift;;
+	--keep-sln)
+	    keep_sln=yes
+	    shift;;
+	--drop-sln)
+	    keep_sln=no
+	    shift;;
+	--keep-ldconfig)
+	    keep_ldconfig=yes
+	    shift;;
+	--drop-ldconfig)
+	    keep_ldconfig=no
 	    shift;;
 	--help)
 	    usage
@@ -131,15 +163,15 @@ fi
 if [ "$keep_locales" != "yes" ]; then
     rm -f "$target"/usr/lib/locale/*
     rm -rf "$target"/usr/share/locale
+    rm -rf "$target"/usr/lib*/gconv
+    rm -f "$target"/usr/bin/localedef
+    rm -f "$target"/usr/sbin/build-locale-archive
 fi
 
 if [ "$keep_docs" != "yes" ]; then
     rm -rf "$target"/usr/share/man
     rm -rf "$target"/usr/share/doc
-fi
-
-if [ "$keep_yum_cache" != "yes" ]; then
-    rm -rf "$target"/var/cache/yum/*
+    rm -rf "$target"/usr/share/info
 fi
 
 if [ "$keep_cracklib" != "yes" ]; then
@@ -156,4 +188,75 @@ if [ "$keep_zoneinfo" != "yes" ]; then
     rm -rf "$target"/usr/share/zoneinfo/*
     mv "$target"/{UCT,UTC,Universal,Zulu,GMT*,*.tab} \
       "$target"/usr/share/zoneinfo/
+fi
+
+if [ "$keep_rpmdb" != "yes" ]; then
+    rm -rf "$target"/var/lib/rpm/*
+fi
+
+if [ "$keep_yum_cache" != "yes" ]; then
+    rm -rf "$target"/var/cache/yum/*
+fi
+
+if [ "$keep_services" != "yes" ]; then
+    rm -f "$target"/etc/services
+    cat > "$target"/etc/services <<'__EOF__'
+tcpmux 1/tcp
+tcpmux 1/udp
+echo 7/tcp
+echo 7/udp
+discard 9/tcp sink null
+discard 9/udp sink null
+ftp 21/tcp
+ftp 21/udp fsp fspd
+ssh 22/tcp
+ssh 22/udp
+telnet 23/tcp
+telnet 23/udp
+smtp 25/tcp mail
+smtp 25/udp mail
+time 37/tcp timserver
+time 37/udp timserver
+nameserver 42/tcp name
+nameserver 42/udp name
+domain 53/tcp
+domain 53/udp
+bootps 67/tcp
+bootps 67/udp
+bootpc 68/tcp dhcpc
+bootpc 68/udp dhcpc
+tftp 69/tcp
+tftp 69/udp
+finger 79/tcp
+finger 79/udp
+http 80/tcp www www-http
+http 80/udp www www-http
+http 80/sctp
+kerberos 88/tcp kerberos5 krb5
+kerberos 88/udp kerberos5 krb5
+pop3 110/tcp pop-3
+pop3 110/udp pop-3
+sunrpc 111/tcp portmapper rpcbind
+sunrpc 111/udp portmapper rpcbind
+auth 113/tcp authentication tap ident
+auth 113/udp authentication tap ident
+ntp 123/tcp
+ntp 123/udp
+imap 143/tcp imap2
+imap 143/udp imap2
+snmp 161/tcp
+snmp 161/udp
+snmptrap 162/tcp
+snmptrap 162/udp snmp-trap
+__EOF__
+fi
+
+if [ "$keep_sln" != "yes" ]; then
+    rm -f "$target"/sbin/sln
+fi
+
+if [ "$keep_ldconfig" != "yes" ]; then
+    rm -f "$target"/sbin/ldconfig
+    rm -f "$target"/etc/ld.so.cache
+    rm -rf "$target"/var/cache/ldconfig/*
 fi
