359718fa6c7ff177ef3413552f57a174d7205102
Erik Nolte <erik_nolte@acm.org>
2011-10-06 14:29:54 -0600

    Checkout gnulib if the directory is empty.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -
 
+if [ -z "$(ls gnulib 2>/dev/null)" ]
+then
+    git clone git://git.savannah.gnu.org/gnulib.git
+fi
+
 ./gnulib/gnulib-tool --update
 
 export AUTOMAKE='automake --foreign --add-missing'
