1f58a3ea47355ed5b5b85e83051921260571e9c7
Pino Toscano <ptoscano@redhat.com>
2015-11-06 15:46:09 +0100

    tests/automake2junit.ml: various refactoring
    - isolate the trs reading in own subroutine
    - use a type for the test result
    - keep a "stack" of the subdirectories traversed, and use that to build
      the classname for each test, using "tests" as base
    - read :guestfs-time: from trs files, if present, and update the tests
      total time with each time

--- tests/automake2junit.ml
+++ tests/automake2junit.ml
@@ -20,6 +20,14 @@
 open Printf
 #load "str.cma"
 
+type test_result =
+  | Pass
+  | Skip
+  | XFail
+  | Fail
+  | XPass
+  | Error
+
 let (//) = Filename.concat
 
 let read_whole_file path =
@@ -42,75 +50,91 @@ let string_charsplit sep =
   Str.split (Str.regexp_string sep)
 
 let find_trs basedir =
-  let rec internal_find_trs basedir =
+  let rec internal_find_trs basedir stack =
     let items = Array.to_list (Sys.readdir basedir) in
-    let items = List.map (fun x -> basedir // x) items in
+    let items = List.map (fun x -> x, basedir // x) items in
     let dirs, files = List.partition (
-      fun x ->
-        try Sys.is_directory x
+      fun (_, full_x) ->
+        try Sys.is_directory full_x
         with Sys_error _ -> false
     ) items in
-    let files = List.filter (fun x -> Filename.check_suffix x ".trs") files in
+    let files = List.filter (fun (x, _) -> Filename.check_suffix x ".trs") files in
+    let files = List.map (fun (_, full_x) -> stack, full_x) files in
     let subdirs_files = List.fold_left (
-      fun acc dir ->
-        (internal_find_trs dir) :: acc
+      fun acc (fn, dir) ->
+        (internal_find_trs dir (fn :: stack)) :: acc
     ) [] dirs in
     let subdirs_files = List.rev subdirs_files in
     List.concat (files :: subdirs_files)
   in
-  internal_find_trs basedir
+  internal_find_trs basedir ["tests"]
 
 let iterate_results trs_files =
   let total = ref 0 in
   let failures = ref 0 in
   let errors = ref 0 in
   let skipped = ref 0 in
+  let total_time = ref 0 in
   let buf = Buffer.create 16384 in
+  let read_trs file =
+    let log_filename = (Filename.chop_suffix file ".trs") ^ ".log" in
+    let content = read_whole_file file in
+    let lines = string_charsplit "\n" content in
+    let testname = ref (Filename.chop_suffix (Filename.basename file) ".trs") in
+    let res = ref Pass in
+    let time = ref 0 in
+    List.iter (
+      fun line ->
+        let line = string_charsplit " " line in
+        (match line with
+        | ":test-result:" :: result :: rest ->
+          let name = String.concat " " rest in
+          if String.length name > 0 then testname := name;
+          res :=
+            (match result with
+            | "PASS" -> Pass
+            | "SKIP" -> Skip
+            | "XFAIL" -> XFail
+            | "FAIL" -> Fail
+            | "XPASS" -> XPass
+            | "ERROR" | _ -> Error);
+        | ":guestfs-time:" :: delta :: _ ->
+          time := int_of_string delta
+        | _ -> ()
+        );
+    ) lines;
+    !testname, !res, !time, log_filename in
   List.iter (
-    fun file ->
-      let rec results file =
-        let content = read_whole_file file in
-        let lines = string_charsplit "\n" content in
-        let log = get_log file in
-        let testname = name_for_test file in
-        List.iter (
-          fun line ->
-            let line = string_charsplit " " line in
-            (match line with
-            | ":test-result:" :: result :: rest ->
-              let name = String.concat " " rest in
-              let name = if String.length name > 0 then name else testname in
-              let print_tag_with_log tag =
-                Buffer.add_string buf (sprintf "  <testcase name=\"%s\" classname=\"TestSuite\" time=\"0\">\n" name);
-                Buffer.add_string buf (sprintf "    <%s><![CDATA[%s]]></%s>\n" tag log tag);
-                Buffer.add_string buf (sprintf "  </testcase>\n")
-              in
-              (match result with
-              | "PASS" ->
-                print_tag_with_log "system-out"
-              | "SKIP" ->
-                skipped := !skipped + 1;
-                print_tag_with_log "skipped"
-              | "XFAIL" | "FAIL" | "XPASS" ->
-                failures := !failures + 1;
-                print_tag_with_log "error"
-              | "ERROR" | _ ->
-                errors := !errors + 1;
-                print_tag_with_log "error"
-              );
-              total := !total + 1
-            | _ -> ()
-            );
-        ) lines;
-      and name_for_test filename =
-        Filename.chop_suffix (Filename.basename filename) ".trs"
-      and get_log filename =
-        let log_filename = (Filename.chop_suffix filename ".trs") ^ ".log" in
-        try read_whole_file log_filename with _ -> ""
+    fun (stack, file) ->
+      let testname, result, time, log_filename = read_trs file in
+      let log =
+        match testname with
+        | _ -> try read_whole_file log_filename with _ -> "" in
+      let print_tag_with_log tag =
+        Buffer.add_string buf (sprintf "  <testcase name=\"%s\" classname=\"%s\" time=\"%d\">\n" testname (String.concat "." (List.rev stack)) time);
+        Buffer.add_string buf (sprintf "    <%s><![CDATA[%s]]></%s>\n" tag log tag);
+        Buffer.add_string buf (sprintf "  </testcase>\n")
       in
-      results file
+      (match result with
+      | Pass ->
+        print_tag_with_log "system-out"
+      | Skip ->
+        skipped := !skipped + 1;
+        print_tag_with_log "skipped"
+      | XFail | Fail | XPass ->
+        failures := !failures + 1;
+        print_tag_with_log "error"
+      | Error ->
+        errors := !errors + 1;
+        print_tag_with_log "error"
+      );
+      total := !total + 1;
+      total_time := !total_time + time
   ) trs_files;
-  Buffer.contents buf, !total, !failures, !errors, !skipped
+  Buffer.contents buf, !total, !failures, !errors, !skipped, !total_time
+
+let sort_trs (_, f1) (_, f2) =
+  compare f1 f2
 
 let () =
   if Array.length Sys.argv < 3 then (
@@ -119,10 +143,10 @@ let () =
   );
   let name = Sys.argv.(1) in
   let basedir = Sys.argv.(2) in
-  let trs_files = List.sort compare (find_trs basedir) in
-  let buf, total, failures, errors, skipped =
+  let trs_files = List.sort sort_trs (find_trs basedir) in
+  let buf, total, failures, errors, skipped, time =
     iterate_results trs_files in
   printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<testsuite name=\"%s\" tests=\"%d\" failures=\"%d\" skipped=\"%d\" errors=\"%d\" time=\"0\">
+<testsuite name=\"%s\" tests=\"%d\" failures=\"%d\" skipped=\"%d\" errors=\"%d\" time=\"%d\">
 %s</testsuite>
-" name total failures skipped errors buf
+" name total failures skipped errors time buf
