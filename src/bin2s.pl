6d76a739853de6b6b2f050525137164bc4ef8839
Richard W.M. Jones <rjones@redhat.com>
2015-12-31 17:01:39 +0000

    bin2s.pl: Add copyright message and license.

--- src/bin2s.pl
+++ src/bin2s.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/perl
+# (C) Copyright 2009-2016 Hilko Bengen.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 # This script creates a source file for the GNU assembler which shuold
 # result in an object file equivalent to that of
