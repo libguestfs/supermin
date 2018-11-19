bece55da108a037eb5e754ce0f132cd3444a3d07
Martin Kletzander <mkletzan@redhat.com>
2018-11-19 23:08:47 +0100

    autogen: Run bootstrap if submodule is not initialized
    This used to be the case before commit 30de2cb603cd.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# If the submodule is not initialized run bootstrap
+if git submodule status .gnulib | grep -q '^-'; then
+    ./bootstrap
+fi
+
 # If no arguments were specified and configure has run before, use the previous
 # arguments
 if test $# -eq 0 && test -x ./config.status; then
